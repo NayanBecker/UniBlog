9e604f3e58af5af6251fb3c3a982bf1c45bf5753
nayanbecker <nayan@unochapeco.edu.br>
2025-09-19 17:26:17 -0300

    update dns

--- blog-Mobile/src/services/api.ts
+++ blog-Mobile/src/services/api.ts
@@ -4,7 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 
 const LOCAL_BASE =
-    Platform.OS === "android" ? "http://192.168.25.165:3333" : "http://192.168.25.165:3333";
+    Platform.OS === "android" ? "http://uniblog.cloud:3333" : "http://uniblog.cloud:3333";
 
 export const api = axios.create({
     baseURL: LOCAL_BASE,
