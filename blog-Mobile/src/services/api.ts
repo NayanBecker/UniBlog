1ee33b697d3b213b32edcd28888e0c54db18d2f7
nayanbecker <nayan@unochapeco.edu.br>
2025-09-25 17:30:20 -0300

    fix url to minio

--- blog-Mobile/src/services/api.ts
+++ blog-Mobile/src/services/api.ts
@@ -4,8 +4,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 
 const LOCAL_BASE =
-    Platform.OS === "android" ? "http://minio.uniblog.cloud:3333" : "http://minio.uniblog.cloud:3333";
+    Platform.OS === "android" ? "http://192.168.25.165:3333" : "http://minio.uniblog.cloud:3333";
 
+export const url = LOCAL_BASE;
 
 export const api = axios.create({
     baseURL: LOCAL_BASE,
@@ -18,8 +19,6 @@ api.interceptors.request.use(async (config) => {
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
-
-    //const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF9BY2NvdW50IjoxNCwiaWF0IjoxNzU3NDU5NTk5LCJleHAiOjE3NTgwNjQzOTl9.VA2I7oDRFaaeN_6K48lSl7pA8q7IzqxoZJf6RfnmJXM'
     return config;
 });
 
