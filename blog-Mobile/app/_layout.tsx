4f9a2675f028cb606fef2a403bf67abafee6d089
nayanbecker <nayan@unochapeco.edu.br>
2025-09-25 23:18:44 -0300

    Feat:Feed de publicações ygN4vSc7/33-feed

--- blog-Mobile/app/_layout.tsx
+++ blog-Mobile/app/_layout.tsx
@@ -1,8 +1,18 @@
-import { Stack } from "expo-router";
+import { useFonts } from "@/src/utils/fonts";
+import { Stack, Slot } from "expo-router";
+import { View } from 'react-native';
 
 export default function RootLayout() {
+    const fontsLoaded = useFonts();
+
+
+    if (!fontsLoaded) {
+        return <View />;
+    }
     return (
         <Stack screenOptions={{ headerShown: false }}>
+            <Slot />
+
             <Stack.Screen name="(auth)" />
             <Stack.Screen name="(app)" />
         </Stack>
