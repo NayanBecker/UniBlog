442251f3b539f9bc57513095e255e95df97c7d54
nayanbecker <nayan@unochapeco.edu.br>
2025-11-03 21:48:05 -0300

    Refactor CourseSelectorComponent to remove unused imports

--- blog-Mobile/src/components/CourseSelectorComponent.tsx
+++ blog-Mobile/src/components/CourseSelectorComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, FlatList, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { Dropdown } from 'react-native-element-dropdown';
 
 const courses = [
