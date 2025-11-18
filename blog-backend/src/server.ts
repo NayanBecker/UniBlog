9b7ed87dfbf74854ccc18e479c644a7e58ea15f8
nayanbecker <nayan@unochapeco.edu.br>
2025-11-17 21:46:57 -0300

    add: console log backend server address

--- blog-backend/src/server.ts
+++ blog-backend/src/server.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 const start = async () => {
   try {
     await app.listen({ port: 3333, host: '0.0.0.0' });
+    console.log('Server is running on http://0.0.0.0:3333');
   } catch (err) {
     console.error(err);
     process.exit(1);
