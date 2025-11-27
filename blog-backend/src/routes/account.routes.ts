0e69d9a4b8d3cb281f56adc3ddfd241b463ce384
nayanbecker <nayan@unochapeco.edu.br>
2025-11-26 21:04:43 -0300

    incluido lib zod to json, para transformar anotações zod em json. documentado rotas para account

--- blog-backend/src/routes/account.routes.ts
+++ blog-backend/src/routes/account.routes.ts
@@ -1,8 +1,21 @@
 import { FastifyInstance } from 'fastify';
 import { registerAccountController, loginAccountController, updateAccountController } from '../controllers/account.controller';
+import { registerAccountSchemaDoc, updateAccountSchemaDoc, authenticateAccountSchemaDoc } from '@/controllers/schemas/account.schema';
 
 export async function authRoutes(app: FastifyInstance) {
-  app.post('/register', registerAccountController);
-  app.post('/login', loginAccountController);
-  app.put('/update/:id_Account', updateAccountController);
+  app.post('/register',
+    {
+      schema: registerAccountSchemaDoc,
+      handler: registerAccountController
+    });
+  app.post('/login',
+    {
+      schema: authenticateAccountSchemaDoc,
+      handler: loginAccountController
+    });
+  app.put('/update/:id_Account',
+    {
+      schema: updateAccountSchemaDoc,
+      handler: updateAccountController
+    });
 }
