be7bcb1a4d014da62b2612769b207d684c0a1198
nayanbecker <nayan@unochapeco.edu.br>
2025-12-09 21:35:24 -0300

    Code Coverage updated

--- blog-Mobile/src/services/api.ts
+++ blog-Mobile/src/services/api.ts
@@ -4,7 +4,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from "expo-router";
 
 const LOCAL_BASE =
-    Platform.OS === "android" ? "http://192.168.3.6:3333" : "http://minio.uniblog.cloud:3333";
+    Platform.OS === "android" ? "https://cd1d8d3ae47e.ngrok-free.app" : "http://minio.uniblog.cloud:3333";
 //Platform.OS === "android" ? "http://uno.uniblog.cloud:3000" : "http://minio.uniblog.cloud:3333";
 
 
